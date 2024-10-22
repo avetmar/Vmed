97f09bc25478bf990e425632372e6a63d3c40da3
Avet <avet.martirosyan.it@gmail.com>
2024-10-22 17:54:17 +0400

    services pages completed

--- js/script.js
+++ js/script.js
@@ -14,3 +14,58 @@ window.addEventListener("scroll", () => {
     header.classList.remove("fixed");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Получаем элемент select после загрузки DOM
+  const select = document.querySelector(".menuSelect");
+
+  // Проверяем, что элемент select существует на странице
+  if (select) {
+    // Добавляем обработчик события "change"
+    select.addEventListener("change", () => {
+      // Обновляем значение при изменении выбора
+      let selectedValue = select.value;
+
+      // Переход на страницу
+      if (selectedValue) {
+        window.location.href = selectedValue;
+      }
+    });
+  }
+});
+
+//scrolling feedbackServices
+const slider = document.querySelector(".feedbackS__slider");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Обработчик для нажатия мыши
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.style.cursor = "grabbing"; // Смена курсора при нажатии
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+// Обработчик для выхода мыши за пределы слайдера
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.style.cursor = "grab"; // Вернуть курсор в исходное состояние
+});
+
+// Обработчик для отпускания мыши
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.style.cursor = "grab"; // Вернуть курсор после скролла
+});
+
+// Обработчик для движения мыши
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; // Если мышь не нажата, выйти из функции
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Скорость скролла
+  slider.scrollLeft = scrollLeft - walk;
+});
